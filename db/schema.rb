15ca48f76e993c7ce4b111efe9fe18fb6eff6ba5
ashraffares <ashraffares090@gmail.com>
2021-03-17 12:11:56 +0200

    Add post model & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_093048) do
+ActiveRecord::Schema.define(version: 2021_03_17_100921) do
 
   create_table "authors", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

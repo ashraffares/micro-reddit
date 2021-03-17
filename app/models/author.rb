1b03a092be3d19cf63d4c5d9ef914359f186c7fe
ngani faith <nganifaith@gmail.com>
2021-03-17 10:55:05 +0100

    add the author model and migrate the db with validation

---
+++ app/models/author.rb
@@ -0,0 +1,6 @@
+class Author < ApplicationRecord
+  validate :username, :email, :password, presence: true
+  validate :username, :email, uniqueness: true
+  validate :username, length: { minimum: 4, maximum: 12 }
+  validate :password, length: { minimum: 6, maximum: 16 }
+end
